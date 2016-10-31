48c634bff0dda41efedb54bf97a6ef981ba9ee3b
Karoid <shj5508@naver.com>
2016-10-31 19:29:21 +0900

    sign_up made

--- public/cafe/js/sign_up.js
+++ public/cafe/js/sign_up.js
@@ -1,11 +1,6 @@
 function submit_action(obj) {
   var regExp = /^(01[016789]{1}|02|0[3-9]{1}[0-9]{1})-?[0-9]{3,4}-?[0-9]{4}$/;
-  // 01로 시작하는 핸드폰 및 지역번호와 050, 070 검증
-  // 원래 050은 0505 평생번호인가 그런데 보편적으로 050-5xxx-xxxx 로 인식함
-  // 0505-xxx-xxxx 라는 식으로 넣으면 통과할 수 없음. 그래서 경고창 띄울때 예시 넣는것이 좋음.
-  // -(하이픈)은 넣어도 되고 생략해도 되나 넣을 때에는 정확한 위치에 넣어야 함.
   if(!obj.val()) {
-    obj.focus();
     return false;
   }
   else if (!regExp.test(obj.val())) {
@@ -25,7 +20,17 @@ $(document).ready(function(){
     }else{
       $('input#tel_check').removeClass("checked").attr("checked",false)
     }
+    if ($('input#password').val() == $('input#password_check').val() && $('input#password_check').val() != "") {
+      $('input#pass_check').addClass("checked").attr("checked",true)
+      $('input#pass2_check').addClass("checked").attr("checked",true)
+    }else{
+      $('input#pass_check').removeClass("checked").attr("checked",false)
+      $('input#pass2_check').removeClass("checked").attr("checked",false)
+    }
   },500)
+  $('.show_info').click(function(){
+    $("iframe").css("display","")
+  })
   $.ajax({
     url: serverip+loginLocation+"sign_up",
     method:"post",
