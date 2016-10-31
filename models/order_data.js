ae2bec91ef7c50b891dfe572f390d896a8d3db74
ajh322 <jihwan322@gmail.com>
2016-11-01 01:12:52 +0900

    주문 db 만듬, 주문 관리 페이지 제작중

--- models/order_data.js
+++ models/order_data.js
@@ -2,16 +2,18 @@
  * Created by AJH322 on 2016-10-31.
  */
 var mongoose = require('mongoose');
+//var User_data = require('./user');
 var userSchema = new mongoose.Schema({
-    order_count: {type: Number}, //총 주문횟수
+    order_count: {type: Number, unique: true, default: 0}, //총 주문횟수 PK
     order_count_today: {type: Number}, //하루의 주문횟수
     order_date: {type: Date}, //주문날짜
     order_item_index: {type: String}, //주문한 제품들 배열로 받을것임.
     order_total_price: {type: Number}, //총 가격
     order_state: {type: String}, //주문상태 주문접수, 주문완료
-    order_personal: {type: String}, //주문한사람의 정보 user.js에서 연동하고 PK 무결성 기능 추가하기
+    order_id: {type: String, required: true}, //주문한사람의 폰번호 KEY로써 삭제되면 이것과 이어진것은 전부다 삭제해야할수도있음
     created_at: {type: Date} //기본
 });
+//회원탈퇴하면 관련데이터 삭제기능 구현??
 userSchema.pre('save', function (next) {
     now = new Date();
     this.updated_at = now;
