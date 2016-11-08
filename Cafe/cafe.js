513a64c5ef00d3100e39a67fc3e403797631b470
ae878 <ae878@naver.com>
2016-11-08 09:32:11 +0900

    modify

--- Cafe/cafe.js
+++ Cafe/cafe.js
@@ -139,6 +139,17 @@ router.get('/sign_up.html', function (req, res) {
         }
     })
 });
+
+router.get('/user_modify.html', function (req, res) {
+    fs.readFile('./Cafe/user_modify.html', 'utf8', function (err, data) {
+        console.log("gg"+req.session.username);
+        User.find({username:req.session.username}).exec(function (err, documents) {
+            console.log(documents);
+            return res.end(ejs.render(data,{data: documents}));
+        
+        })
+})
+});
 router.get('/QnA.html/:page?', function (req, res) {
     fs.readFile('./Cafe/QnA.html', 'utf8', function (err, data) {
         try {
@@ -520,8 +531,7 @@ router.post("/nonusersign_up", function (req, res) {
 })
 //회원 탈퇴
 router.post("/sign_out", function (req, res) {
-    console.log(req.session.username + " is sign out");
-    console.log(req.body);
+   
     User.find({username:req.session.username }).exec(function (err, documents) {
        
     req.session.destroy();  // 세션 삭제
@@ -532,6 +542,31 @@ router.post("/sign_out", function (req, res) {
         });
     
        
+})
+
+
+//회원 수정
+router.post("/user_modify", function (req, res) {
+   
+    console.log(JSON.stringify(req.body) + "user_modify attempt");
+  
+    
+    User.update(
+            {username: req.session.username},
+            {
+                $set: {
+                     username: req.body.username,
+        password: req.body.password,
+        realname: req.body.realname,
+        address: req.body.address
+                }
+            },
+            function (err, numberAffected, rawResponse) {
+                //console.log("에러:"+err+"영향"+numberAffected+"raw"+rawResponse);
+                res.redirect("../cafe/main.html");
+            })
+    // save user to database 회원가입 부분 최초저장부분
+    
 })
 module.exports = router;
 
